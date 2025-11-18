0e2af6697968da85e941ba00bc53d37abed63e5b
Abhilash Rudra <rudraabhilash@users.noreply.github.com>
2025-11-18 17:28:53 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+agent = WebAgent()
+
+agent.go("https://www.linkedin.com/jobs/collections/recommended")
+
+#agent.click(text="Jobs")
+#agent.click(selector="a.more-info")
+#agent.type("#searchBox", "mobile phones")
+#agent.extract("""
+#Extract the main heading and return JSON:
+#{ "heading": "" }
+#""")
+
+# agent.task("""
+# 1. Go to https://www.flipkart.com
+# 2. Search for 'laptop'
+# 3. Extract top 5 results: name, price and rating.
+# 4. Return JSON list.
+# """)
+
+
+
+
+
